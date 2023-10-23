d5ce6cf3279d295cd46a6608fe99817af789f398
tcotav <931363+tcotav@users.noreply.github.com>
2023-10-22 19:37:31 -0700

    added basic viper config

--- cmd/log/main.go
+++ cmd/log/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 
+	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
+	"github.com/spf13/viper"
 	"github.com/tcotav/logtest/data"
 	"github.com/tcotav/logtest/middleware"
 )
@@ -14,7 +18,35 @@ const (
 	ServiceVersion = "0.1.0"
 )
 
+func initViper() {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
+
+	if err != nil { // exit on failed config file read
+		log.Panic().Err(err).Msg("Error while reading config file")
+	}
+}
+
 func main() {
+
+	// set up viper config
+	initViper()
+	port := viper.GetInt("port")
+	loglevel := viper.GetString("loglevel")
+
+	// set up zerolog log level -- removed a few levels
+	switch loglevel {
+	case "debug":
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	case "info":
+		zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	case "error":
+		zerolog.SetGlobalLevel(zerolog.ErrorLevel)
+	default:
+		zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	}
 
 	// set up gin
 	ginMode := "release"
@@ -24,6 +56,8 @@ func main() {
 	router.Use(middleware.RequestLogger())
 	// set up otelgin middleware
 	router.Use(gin.Recovery())
+
+	log.Debug().Str("App", ServiceName).Str("Call", "main").Msg("Testing debug and loglevel")
 
 	// hack in some dummy handler
 	router.GET("/ping", func(c *gin.Context) {
@@ -40,6 +74,6 @@ func main() {
 		})
 	})
 
-	log.Info().Str("App", ServiceName).Str("Call", "main").Msg("Starting server on port 8080")
-	router.Run(":8080")
+	log.Info().Str("App", ServiceName).Str("Call", "main").Msgf("Starting server on port %d", port)
+	router.Run(fmt.Sprintf(":%d", port))
 }
